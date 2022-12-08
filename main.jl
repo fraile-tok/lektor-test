f4d6879c94b19c0b1c3b3baa88e15f286302b0d8
José Pablo Iglesias <sir.jpiglesias@gmail.com>
2022-12-08 07:29:14 -0600

    Filer Empty Lines + Maintains Numbering

--- main.jl
+++ main.jl
@@ -42,43 +42,48 @@ function searchgenes(files, genes, resultsname) # searchgenes(array of files, ar
         # println("File ", f, ": ")
         write(resultsfile, "\n  File ", string(f), ":")
         global l = 1
-        filter!(!isempty, data_by_line)
+       # filter!(!isempty, data_by_line) # simply deletes the line from array, but does not maintain numbering
         # PER LINE
         for line in data_by_line
-            linescore = 0
-            # cleanup
-            line_lc = lowercase(line)
-            line_sans_g = replace(line_lc, "-" => "")
-            line_sans_d = replace(line_sans_g, "." => "")
-            line_clean = line_sans_d
-            #println("Paragraph ", l, ":\n")
-            write(resultsfile, "\n    Paragraph ", string(l), ": ")
-            for gene in genes
-                filter!(!ismissing, gene) # deletes missing values from gene arrays (aliases lists)
-                genescore = 0 
-                aliases = gene
-                for alias in aliases
-                    alias_sans_dash = replace(alias, "-" => "")
-                    alias_sans_dot = replace(alias_sans_dash, "." => "")
-                    alias_clean = lowercase(alias_sans_dot)
-                    occurs = occursin(alias_clean, line_clean)
-                    if (occurs == true)
-                        genescore += 1
-                        linescore += 1
+            # if line is empty then skip
+            if (line == "") 
+                global l += 2 # the line is skipped but the .txt paragraph numbers maintained
+            else
+                linescore = 0
+                # cleanup
+                line_lc = lowercase(line)
+                line_sans_g = replace(line_lc, "-" => "")
+                line_sans_d = replace(line_sans_g, "." => "")
+                line_clean = line_sans_d
+                #println("Paragraph ", l, ":\n")
+                write(resultsfile, "\n    Paragraph ", string(l), ": ")
+                for gene in genes
+                    filter!(!ismissing, gene) # deletes missing values from gene arrays (aliases lists)
+                    genescore = 0 
+                    aliases = gene
+                    for alias in aliases
+                        alias_sans_dash = replace(alias, "-" => "")
+                        alias_sans_dot = replace(alias_sans_dash, "." => "")
+                        alias_clean = lowercase(alias_sans_dot)
+                        occurs = occursin(alias_clean, line_clean)
+                        if (occurs == true)
+                            genescore += 1
+                            linescore += 1
+                        end
+                    end
+                    if (genescore >= 1)
+                        #println(gene[1], ": ", score)
+                        write(resultsfile, "\n      ", gene[1], ": ", string(genescore))
                     end
                 end
-                if (genescore >= 1)
-                    #println(gene[1], ": ", score)
-                    write(resultsfile, "\n      ", gene[1], ": ", string(genescore))
+                if (linescore >= 1)
+                    write(resultsfile, "")
+                else 
+                    write(resultsfile, "No genes found.")
                 end
+                #println("\n")
+                global l += 1
             end
-            if (linescore >= 1)
-                write(resultsfile, "")
-            else 
-                write(resultsfile, "No genes found.")
-            end
-            #println("\n")
-            global l += 1
         end
         global l = 1
         global f += 1
