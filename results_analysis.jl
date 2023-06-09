354a90adfe72d9e8b1de05baa6b842bc27359aba
José Pablo Iglesias <sir.jpiglesias@gmail.com>
2023-06-08 20:33:51 -0600

    Analysis Change

--- results_analysis.jl
+++ results_analysis.jl
@@ -3,7 +3,7 @@ using DataFrames
 using Combinatorics
 
 # Read the CSV file into a DataFrame
-df = CSV.read("results/analysis_test.csv", DataFrame)
+df = CSV.read("results/results_twos.csv", DataFrame)
 
 # Get the names of the genes from the DataFrame
 genes = names(df)[1:end-2]
@@ -11,22 +11,6 @@ genes = names(df)[1:end-2]
 # Create a dictionary to store the gene pair counts
 pair_counts = Dict()
 
-#A
-# Iterate over each row in the DataFrame
-for row in eachrow(df)
-    # Iterate over each pair of genes
-    for i in 1:length(genes)-1
-        for j in i+1:length(genes)
-            # Create the gene pair as a string
-            pair = string(genes[i], ":", genes[j])
-            
-            # Increment the count for the gene pair in the dictionary
-            pair_counts[pair] = get(pair_counts, pair, 0) + (row[genes[i]] == 1 && row[genes[j]] == 1)
-        end
-    end
-end
-
-#B
 # Iterate over each row in the DataFrame
 for row in eachrow(df)
     # Create a set to store the active genes for the current row
@@ -54,14 +38,9 @@ end
 
 filtered_pair_counts = Dict()
 
-# Filter out pairs with count 0
+# Print out pairs with count > 0
 for (pair, count) in pair_counts
     if count > 0
-        filtered_pair_counts[pair] = count
+        println("$pair: $count instance(s)")
     end
-end
-
-# Print the gene pair counts
-for (pair, count) in filtered_pair_counts
-    println("$pair: $count instance(s)")
 end
